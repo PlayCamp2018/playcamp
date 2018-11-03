5cd2284c90dfc68b2b118af8264cf3dba9608200
peter fischer <pefi@pfx220.noname.lan>
2018-11-03 05:45:02 +0100

    nen UserTest ;)

--- test/models/DBTest/UserTest.java
+++ test/models/DBTest/UserTest.java
@@ -1,5 +1,44 @@
 package models.DBTest;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class UserTest extends  Unit {
+//import org.jongo.Jongo;
+//import org.jongo.MongoCollection;
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.github.fakemongo.Fongo;
+//import com.mongodb.DB;
+
+public class UserTest {
+
+    @Test
+    public void test_user_001() {
+        Users u = new Users( new org.bson.types.ObjectId() ,
+                "FirstName1 ", "LastName1", "Test@mail.lan");
+        assertEquals ( u.getFirstname(), "FirstName1 ");
+
+
+    }
+
+    @Test
+    public void test_user_mail_001() {
+        try{
+
+            Users u = new Users( new org.bson.types.ObjectId() ,
+                    "FirstName1 ", "LastName1", "keine.Mailadresse");
+
+            assertTrue("Das ist keine gültige mailadresse ;)",false);
+        }catch( Exception e){
+            assertTrue("Wrong Mailadress", true);
+        }
+
+    }
+
+
+}
+
+
+
+
+
