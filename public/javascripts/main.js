535e2b4e9094787a9deaad93f0c5c90a2ad64fbf
peter fischer <s0539130@htw-berlin.de>
2018-11-10 20:28:26 +0100

    mit buttons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,10 +5,24 @@ $( document ).ready(function() {
 $( window ).on( "load", function() {
     console.log( "window loaded" );
 
-    $.get('/login', function(response) {
+/*    $.get('/login', function(response) {
         //console.log(response);
         document.getElementById("login").innerHTML = response;
+    });*/
+
+    $( "#target" ).click(function() {
+        $.get('/login', function(response) {
+            //console.log(response);
+            document.getElementById("login").innerHTML = response;
+        });
     });
+    $( "#other" ).click(function() {
+        $.get('/lang', function(response) {
+            //console.log(response);
+            document.getElementById("login").innerHTML = response;
+        });
+    });
+
 });
 
 
