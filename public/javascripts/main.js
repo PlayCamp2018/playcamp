9fb8941767d3b1d59b366ee716d27aeea1c5cefc
peter fischer <s0539130@htw-berlin.de>
2018-11-11 21:47:33 +0100

    ajax by jsRouten ...

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,10 +5,6 @@ $( document ).ready(function() {
 $( window ).on( "load", function() {
     console.log( "window loaded" );
 
-/*    $.get('/login', function(response) {
-        //console.log(response);
-        document.getElementById("login").innerHTML = response;
-    });*/
 
     $( "#target" ).click(function() {
         $.get('/login', function(response) {
@@ -21,6 +17,20 @@ $( window ).on( "load", function() {
             //console.log(response);
             document.getElementById("login").innerHTML = response;
         });
+    });
+
+    $( "#btn-routen" ).click(function() {
+        $.ajax(jsRoutes.controllers.DBTestController.listUser())
+            .done(function( data ) {
+                if ( console && console.log ) {
+                    console.log( "Sample of data:", data.slice( 0, 100 ) );
+                    document.getElementById("login").innerHTML = data;
+                    console.log("done");
+                }
+            })
+            .fail(
+                console.log("fail")
+            );
     });
 
 });
