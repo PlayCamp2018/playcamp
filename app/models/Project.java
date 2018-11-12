6a01096b2b1380788f537fc9d9312b60feadb05a
TreeLow <s0567293@htw-berlin.de>
2018-11-12 16:25:05 +0100

    DatenModell

---
+++ app/models/Project.java
@@ -0,0 +1,85 @@
+package models.DBTest;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.types.ObjectId;
+import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Project {
+
+    public Project() {}
+
+    @JsonCreator
+    public Project(@JsonProperty("id") ObjectId id,
+                   @JsonProperty("name") String name,
+                   @JsonProperty("beschreibung") String beschreibung,
+                   @JsonProperty("ownerid") ObjectId ownerid,
+                   @JsonProperty("userList") List <ObjectId> userList,
+                   @JsonProperty("messageBoardID") ObjectId messageBoardID
+                   ) {
+        this.id = id;
+        this.name = name;
+        this.beschreibung = beschreibung;
+        this.ownerid = ownerid;
+        this.userList = userList;
+        this.messageBoardID = messageBoardID;
+    }
+
+    @JsonProperty("id")
+    public ObjectId id;
+    @JsonProperty("name")
+    public String name;
+    @JsonProperty("beschreibung")
+    public String beschreibung;
+    @JsonProperty("ownerid")
+    public ObjectId ownerid;
+    @JsonProperty("userList")
+    public List<ObjectId> userList = new ArrayList<ObjectId>();
+    @JsonProperty("messageBoardID")
+    public ObjectId messageBoardID;
+
+
+    public ObjectId getId() {
+        return id;
+    }
+    public String getName() {
+        return  name;
+    }
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+    public ObjectId getOwnerid() { return ownerid; }
+    public List<ObjectId> getUserList() {
+        return userList;
+    }
+    public ObjectId getMessageBoardID() {
+        return messageBoardID;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public void setOwnerid(ObjectId ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public void setUserList(List<ObjectId> userList) {
+        this.userList = userList;
+    }
+
+    public void setMessageBoardID(ObjectId messageBoardID) {
+        this.messageBoardID = messageBoardID;
+    }
+}
---
+++ app/models/User.java
@@ -0,0 +1,89 @@
+package models.DBTest;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.types.ObjectId;
+
+
+public class User {
+
+    public User() {}
+
+    @JsonCreator
+    public User(@JsonProperty("id") ObjectId id,
+                @JsonProperty("firstname") String firstname,
+                @JsonProperty("lastname")String lastname,
+                @JsonProperty("password")String password,
+                @JsonProperty("email") String email,
+                @JsonProperty("profilepicID") ObjectId profilePicID) {
+
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.email = email;
+        this.profilePicID = profilePicID;
+    }
+
+    @JsonProperty("id")
+    public ObjectId id;
+    @JsonProperty("firstname")
+    public String firstname;
+    @JsonProperty("lastname")
+    public String lastname;
+    @JsonProperty("password")
+    public String password;
+    @JsonProperty("email")
+    public String email;
+    @JsonProperty("profilePicID")
+    public ObjectId profilePicID;
+
+
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public ObjectId getProfilePicID() {
+        return profilePicID;
+    }
+
+    public void setId(ObjectId _id) {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setProfilePicID(ObjectId profilePicID) {
+        this.profilePicID = profilePicID;
+    }
+}
