ecb2bde4824ac6274bfa83d998d0008e61448dff
Fabian-Ket <fabian.ket@gmail.com>
2018-11-21 16:00:20 +0100

    [update] add POST request + JSON Body and project name and description update

--- app/controllers/ProjectController.java
+++ app/controllers/ProjectController.java
@@ -1,17 +1,17 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import models.Database.Project;
 import models.Database.User;
 import org.bson.types.ObjectId;
 import play.mvc.Controller;
 import play.mvc.Result;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectController extends Controller {
 
-    public Result edit() {
+    public Result index() {
 
         Project pro = Project.TestProjects.projects.get(0);
 
@@ -22,6 +22,23 @@ public class ProjectController extends Controller {
         for (ObjectId id : pro.getUserList()) { proUsers.add(Project.TestProjects.findUserById(id)); }
 
         return ok(views.html.Project.editProject.render(pro, proOwner, proUsers));
+    }
+
+    public Result edit() {
+        // TODO: make change, send index back or so
+        JsonNode json = request().body().asJson();
+
+        String projectName = json.get("editProject_name").textValue();
+        String projectDes = json.get("editProject_description").textValue();
+
+        Project pro = Project.TestProjects.projects.get(0);
+        pro.setName(projectName);
+        pro.setBeschreibung(projectDes);
+
+        // here
+
+        //
+        return index();
     }
 
 }
--- public/javascripts/editProject.js
+++ public/javascripts/editProject.js
@@ -5,3 +5,30 @@ $('body').on('click','#editProject_btn_reset',function(){
     });
 });
 
+$('body').on('click', '#editProject_btn_ok', function() {
+
+    // var $button = $( this );
+
+    // $button.width( $button.width() ).text('...');
+
+    // set ajax data
+    var data = {
+        'editProject_name' : $('#editProject_name').val(),
+        'editProject_description' : $('#editProject_description').val()
+    };
+
+    $.ajax ({
+         url: '/editProject', // url
+         type: "POST", // send method
+         data: JSON.stringify(data), // json send data
+         dataType: "html", // return type
+         contentType: "application/json; charset=utf-8", // send type
+         success: function(response) { // success callback
+            console.log('ok');
+            document.getElementById("editProject_content").innerHTML = response;
+         },
+         error: function(response) { // error callback
+            console.log('error')
+         }
+    });
+});
